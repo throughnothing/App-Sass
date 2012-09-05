b7837849ef9f7bf8d6a0d8572ff8e4334056698a
William Wolf <throughnothing@gmail.com>
2012-09-04 17:19:09 -0700

    Initial commit.

--- lib/App/Sass.pm
+++ lib/App/Sass.pm
@@ -2,6 +2,17 @@ use strict;
 use warnings;
 package App::Sass;
 
-# ABSTRACT:
+# ABSTRACT: sass command-line tool modeled after the ruby's version
+
+1;
+
+=pod
+
+=head1 SYNOPSIS
+
+Check out L<sassp> for more info.
+
+=cut
+
 
 1;
